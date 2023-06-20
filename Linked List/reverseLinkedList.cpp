9fff67612eeba001b4d51544f488fa46acbb1ac5
DivyamaniNidhi <divyamaninidhi04@gmail.com>
2023-06-20 21:05:07 +0530

    changes

---
+++ Linked List/reverseLinkedList.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+
+class Node {
+public:
+    int data;
+    Node* next;
+
+    // Constructor
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+
+    // Destructor
+    ~Node() {
+        if (this->next != NULL) {
+            delete next;
+            this->next = NULL;
+        }
+    }
+};
+
+void push(Node* &head, int data) {
+    Node* newNode = new Node(data);
+    if (head == NULL) {
+        head = newNode;
+    } else {
+        Node* temp = head;
+        while (temp->next != NULL) {
+            temp = temp->next;
+        }
+        temp->next = newNode;
+    }
+}
+
+
+void print(Node* head) {
+    Node* temp = head;
+    while (temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+void reverse(Node* &head) {
+    if (head == NULL || head->next == NULL) {
+        return;
+    }
+    Node* prev = NULL;
+    Node* curr = head;
+    Node* forward = NULL;
+
+    while (curr != NULL) {
+        forward = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = forward;
+    }
+    head = prev;
+}
+
+int main() {
+    Node* head = NULL;
+    push(head, 10);
+    push(head, 20);
+    push(head, 30);
+    push(head, 40);
+    push(head, 50);
+    cout << "Given Linked List: ";
+    print(head);
+
+    reverse(head);
+    cout << "Reversed Linked List: ";
+    print(head);
+
+    return 0;
+}
