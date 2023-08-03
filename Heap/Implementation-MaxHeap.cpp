7695dfc5e5de3fb4291bb1ce6c90c6ff5dc90a25
DivyamaniNidhi <divyamaninidhi04@gmail.com>
2023-08-03 23:15:21 +0530

    commit

--- Heap/Implementation-MaxHeap.cpp
+++ Heap/Implementation-MaxHeap.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<queue>
 using namespace std;
 
 class heap {
@@ -66,16 +67,27 @@ void heapify(int arr[], int n, int i) {
     int left = 2*i;
     int right = 2*i+1;
 
-    if(left < n && arr[largest] < arr[left]) {
+    if(left <= n && arr[largest] < arr[left]) {
         largest = left;
     }
-    if(right < n && arr[largest] < arr[right]) {
+    if(right <= n && arr[largest] < arr[right]) {
         largest = right;
     }
 
     if(largest != i) {
         swap(arr[i], arr[largest]);
         heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n) {
+    int size = n;
+    while(size > 1) {
+        //step 1 - swap root node with node at last index
+        swap(arr[1], arr[size]);
+        size--;
+        //step 2 - place root node at correct position
+        heapify(arr, size, 1); 
     }
 }
 
@@ -101,7 +113,41 @@ int main () {
     }
     cout<<"After Heapify: "<<endl;
     for(int i = 1; i <= n; i++) {
-        cout<<arr[i]<<" ";
+        cout<<arr[i]<<" "; // 55 53 54 52 50
     }
     cout<<endl;
+
+    //heap sort
+    heapSort(arr, n);
+    cout<<"After Heap Sort: "<<endl;
+    for(int i = 1; i <= n; i++) {
+        cout<<arr[i]<<" "; // 50 52 53 54 55
+    }
+    cout<<endl;
+
+    cout<<"Using Priority Queue STL:"<<endl;
+    //max heap
+    priority_queue<int> pq;
+    pq.push(4);
+    pq.push(2);
+    pq.push(5);
+    pq.push(1);
+
+    cout<<"Element at top: "<<pq.top()<<endl;
+    pq.pop();
+    cout<<"Element at top: "<<pq.top()<<endl;
+
+
+    //min heap
+    priority_queue<int, vector<int>, greater<int>> minheap;
+
+    minheap.push(4);
+    minheap.push(2);
+    minheap.push(5);
+    minheap.push(1);
+
+    cout<<"Element at top: "<<minheap.top()<<endl;
+    minheap.pop();
+    cout<<"Element at top: "<<minheap.top()<<endl;
+
 }
